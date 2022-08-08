ec0ea231761f551665a6a27ea9362dbce3ab8043
DESKTOP-KVR2LMS\HP <moizrehman.mr@gmail.com>
2022-08-08 17:15:55 +0500

    no message

--- src/views/Item/item.js
+++ src/views/Item/item.js
@@ -42,7 +42,7 @@ function Item() {
 
                     </div>
                     <div className="productDescription">
-                        <p className="p-bold">Product Desc</p>
+                        <p className="p-bold">Product Description</p>
                         <br/>
                         <p>{Description}</p>
 
